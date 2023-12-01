009111a11665e7c21491e1f829022afdb9938b41
Marek Sośnicki <marek.sosnicki@deliveroo.com>
2023-12-01 10:28:56 +0100

    Day 1 part 1

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+## Requirements
+
+1. Install https://github.com/scarvalhojr/aoc-cli/ and configure you session cookie.
+2. 
--- examples/day_01.rs
+++ examples/day_01.rs
@@ -1,3 +1,14 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello day1");
+    println!("Hello day 1!");
+    let input = read_to_string("input").unwrap();
+
+    let result : usize = input.lines().map(|line| {
+        let first = line.chars().find(|c| c.is_digit(10)).unwrap();
+        let last = line.chars().rfind(|c| c.is_digit(10)).unwrap();
+        println!("first '{first}' last '{last}'");
+        (first as usize - '0' as usize) * 10 + (last as usize - '0' as usize)
+    }).sum();
+    println!("Result: {}", result);
 }
